9cc6598455e0ba78035ab08a330fbb4e1255cd27
Barrdo <136634129+Barrdo@users.noreply.github.com>
2024-04-23 08:50:32 -0300

    script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,69 +1,77 @@
-let pokemonName = document.querySelector('.pokemon_name');
-const pokemonNumber = document.querySelector('.pokemon_number');
-const pokemonImage = document.querySelector('.pokemon_image');
-const form = document.querySelector('.form');
-const input = document.querySelector('.input_search');
-const buttonPrev = document.querySelector('.btn-prev');
-const buttonNext = document.querySelector('.btn-next');
+    //Variaveis
+    let pokemonName = document.querySelector('.pokemon_name');
+    let pokemonNumber = document.querySelector('.pokemon_number');
+    let pokemonImage = document.querySelector('.pokemon_image');
+    const form = document.querySelector('.form');
+    let input = document.querySelector('.input_search');
+    const buttonPrev = document.querySelector('.btn-prev');
+    const buttonNext = document.querySelector('.btn-next');
 
-let searchPokemon = 1;
+    //define id inicial
+    let searchPokemon = 1;
 
-const fetchPokemon = async (pokemon) => {
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    //linka API
+    const fetchPokemon = async (pokemon) => {
+        const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    if (APIResponse === 200){
+        //verifica se a API está dando resposta
+        if (APIResponse.status === 200){
+            const data = await APIResponse.json();
+            return data;
+        }
         const data = await APIResponse.json();
         return data;
-    }
-    const data = await APIResponse.json();
-    return data;
-};
-const renderPokemon = async (pokemon) => {
+    };
 
-    pokemonName.innerHTML = 'Loading...'
-    pokemonNumber.innerHTML = ''
-    const data = await fetchPokemon(pokemon);
+    //Busca o pokemon do pokemon
+    const renderPokemon = async (pokemon) => {
 
-    if(data){
+        pokemonName.innerHTML = 'Carregando...'
+        pokemonName.innerHTML = ''
+        const data = await fetchPokemon(pokemon);
 
-        console.log(data)
-        pokemonImage.style.display = 'block'
-        console.log(data.id)
-        pokemonName.innerHTML = data.name;
-        pokemonNumber.innerHTML = data.id;
-        console.log(data['sprites']['versions']['generation-v']['black-white']['animated']['front-default'])
-        pokemonImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/${data.id}.png`
-        input.value = '';
-        searchPokemon = data.id;
-    } else {
-        pokemonImage.style.display = 'none'
-        pokemonName = 'Not Found:('
-        pokemonNumber = ''
-    }
-    
-};
+        if(data){
 
-form.addEventListener('submit', (event) => {
+            //renderiza imagem, ID e Nome do pokemon 
+            pokemonImage.style.display = 'block'
+            pokemonNumber.innerHTML = data.id;
+            pokemonName.innerHTML = data.name;
+            pokemonImage.src = data.sprites.versions['generation-v']['black-white'].animated['front_default'];
+            input.value = '';
+            searchPokemon = data.id;
+        } else {
+            //caso não ache o pokemon
+            pokemonImage.style.display = 'none'
+            pokemonName.innerHTML = 'Não encontrado:('
+            pokemonNumber.innerHTML = ''
+        }
+        
+    };
 
-    event.preventDefault(); 
-    renderPokemon(input.value.toLowerCase())
-    
+    //deixa as letras minusculas
+    form.addEventListener('submit', (event) => {
 
-})
+        event.preventDefault(); 
+        renderPokemon(input.value.toLowerCase())
+        
 
-buttonPrev.addEventListener('click', () => {
-    if (searchPokemon > 1){
-        searchPokemon - 1;
+    })
+
+    //volta um ID
+    buttonPrev.addEventListener('click', () => {
+        if (searchPokemon > 1){
+            searchPokemon -= 1;
+            renderPokemon(searchPokemon);
+        }
+        
+    })
+
+    //Avança um ID
+    buttonNext.addEventListener('click', () => {
+
+        searchPokemon += 1;
         renderPokemon(searchPokemon);
-    
-    
-}})
 
-buttonNext.addEventListener('click', () => {
+    })
 
-    searchPokemon += 1;
-    renderPokemon(searchPokemon);
-
-})
-
-renderPokemon(searchPokemon)
+    renderPokemon(searchPokemon)
